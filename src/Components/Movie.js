6a6415ed0ea9714e45ef12a9c3145fbdb9cc8653
Leandre17 <leandre.godet@epitech.eu>
2024-12-15 17:59:43 +0100

    [FIX] movie_title

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,56 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import fetchData from "./FetchData";
-import SimilarMovies from "./SimilarMovie";
 
-function GetMovieActor({ movie_Id }) {
-  const navigate = useNavigate();
-  const [Actors, setActors] = useState([]);
-  useEffect(() => {
-    fetchData(`api/Actor/movie/${movie_Id}`)
-      .then((data) => setActors(data.items))
-      .catch(console.error);
-  }, [movie_Id]);
-  if (!Actors) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      {Actors.map((actor) => (
-        <button onClick={() => navigate(`/actor/${actor.nconst}`)}>
-          {actor.primaryname}
-        </button>
-      ))}
-    </div>
-  );
-}
-
-function MovieDetails({ movie_Id }) {
-  const [movie, setMovie] = useState(null);
-  useEffect(() => {
-    fetchData(`api/MovieDetails/${movie_Id}`)
-      .then(setMovie)
-      .catch(console.error);
-  }, [movie_Id]);
-
-  if (!movie) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <>
-      <div>
-        <h1>{movie.movie_Title}</h1>
-        <p>Start Year: {movie.startyear}</p>
-        <p>End Year: {movie.endyear}</p>
-        <p>Genre: {movie.genres}</p>
-        <GetMovieActor movie_Id={movie_Id} />
-      </div>
-      <div>
-        <SimilarMovies movie_Id={movie_Id} />
-      </div>
-    </>
-  );
-}
 
 function Movie({ movie }) {
   const navigate = useNavigate();
--- src/routes/MoviePage.js
+++ src/routes/MoviePage.js
@@ -1,12 +1,63 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { MovieDetails } from "../Components/Movie";
+import { useState, useEffect } from "react";
+import fetchData from "../Components/FetchData";
+import SimilarMovies from "../Components/SimilarMovie";
+
+function GetMovieActor({ movie_Id }) {
+  const navigate = useNavigate();
+  const [Actors, setActors] = useState([]);
+  useEffect(() => {
+    fetchData(`api/Actor/movie/${movie_Id}`)
+      .then((data) => setActors(data.items))
+      .catch(console.error);
+  }, [movie_Id]);
+  if (!Actors) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      {Actors.map((actor) => (
+        <button onClick={() => navigate(`/actor/${actor.nconst}`)}>
+          {actor.primaryname}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function MovieDetails({ movie_Id }) {
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    fetchData(`api/MovieDetails/${movie_Id}`)
+      .then(setMovie)
+      .catch(console.error);
+  }, [movie_Id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <div>
+        <h1>{movie.primarytitle || movie.originaltitle}</h1>
+        <p>Start Year: {movie.startyear}</p>
+        <p>End Year: {movie.endyear}</p>
+        <p>Genre: {movie.genres}</p>
+        <GetMovieActor movie_Id={movie_Id} />
+      </div>
+      <div>
+        <SimilarMovies movie_Id={movie_Id} />
+      </div>
+    </>
+  );
+}
+
 const MoviePage = () => {
   const { movie_Id } = useParams();
 
   return (
     <div>
-      <h1>Movie Page</h1>
       <MovieDetails movie_Id={movie_Id} />
     </div>
   );
